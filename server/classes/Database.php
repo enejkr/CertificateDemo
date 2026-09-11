c91dc8b7d7ed1e31682d0e41dbec036a24f1637f
enejkr <student.marketing@int.kronoterm.com>
2026-09-11 14:51:23 +0200

    popravil in dopolnil Database Class

--- server/classes/Database.php
+++ server/classes/Database.php
@@ -13,10 +13,23 @@ class Database
 
         $dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
 
-        $this->pdo = new PDO($dsn, $user, $pass, [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-        ]);
+        try {
+            $this->pdo = new PDO($dsn, $user, $pass, [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ]);
+        } catch (PDOException $e) {
+            http_response_code(500);
+
+            header('Content-Type: application/json');
+
+            echo json_encode([
+                'success' => false,
+                'message' => 'Database connection failed'
+            ]);
+
+            exit;
+        }
     }
 
     public function getConnection(): PDO
