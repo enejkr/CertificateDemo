8eda51d962d8df0a3647c5ac81e3b25f9b6862cd
enejkr <enejkr@gmail.com>
2026-09-14 11:15:54 +0200

    spremeba namesto users tabele je celotna baza popravek izvoza

--- mtls_demo.sql
+++ mtls_demo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gostitelj: 127.0.0.1
--- Čas nastanka: 14. sep 2026 ob 10.51
+-- Čas nastanka: 14. sep 2026 ob 11.12
 -- Različica strežnika: 10.4.32-MariaDB
 -- Različica PHP: 8.2.12
 
@@ -24,6 +24,21 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktura tabele `refresh_tokens`
+--
+
+CREATE TABLE `refresh_tokens` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `token_hash` char(64) NOT NULL,
+  `expires_at` datetime NOT NULL,
+  `revoked_at` datetime DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Struktura tabele `users`
 --
 
@@ -34,17 +49,21 @@ CREATE TABLE `users` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Odloži podatke za tabelo `users`
---
 
-INSERT INTO `users` (`id`, `username`, `certificate_fingerprint`, `created_at`) VALUES
-(19, 'Mobile', '3AD719AFC39227CE1382BD3EA7DE944928ABD650644AA0CA8136F9500BDC3711', '2026-09-11 12:18:35'),
-(20, 'Produkcija', '71F2266AD76E9BC4E92F9C5B1AC41FFBB9903B03724C223465F42735035E3FC5', '2026-09-11 12:19:18');
 
 --
 -- Indeksi zavrženih tabel
 --
+
+--
+-- Indeksi tabele `refresh_tokens`
+--
+ALTER TABLE `refresh_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `token_hash` (`token_hash`),
+  ADD KEY `idx_refresh_tokens_user_id` (`user_id`),
+  ADD KEY `idx_refresh_tokens_expires_at` (`expires_at`),
+  ADD KEY `idx_refresh_tokens_revoked_at` (`revoked_at`);
 
 --
 -- Indeksi tabele `users`
@@ -59,10 +78,26 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT tabele `refresh_tokens`
+--
+ALTER TABLE `refresh_tokens`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=162;
+
+--
 -- AUTO_INCREMENT tabele `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- Omejitve tabel za povzetek stanja
+--
+
+--
+-- Omejitve za tabelo `refresh_tokens`
+--
+ALTER TABLE `refresh_tokens`
+  ADD CONSTRAINT `fk_refresh_tokens_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
